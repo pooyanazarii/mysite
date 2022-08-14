84518267006ed95e88bc4f1ae17018b0d83f07a6
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-14 15:27:20 +0430

    add json page
    add json page to project

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 def http_test(request):
     return HttpResponse('<h1>http-test</h1>')
 
+def json_test(request):
+    return JsonResponse({"firstname":"Pooya","lastname":"Nazari"})
