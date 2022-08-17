ae89f3e0869cf4ae1227530abb4bf76b89d00560
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-18 01:04:13 +0430

    Added Templates file and dynamic addres
    -added templates file
    -customize base header and footer
    -edit url to dynamic urls

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'websit.apps.WebsitConfig'
+    'websit.apps.WebsitConfig',
+    # 'blog.apps.BlogConfig'
+    'blog'
 ]
 
 MIDDLEWARE = [
--- websit/urls.py
+++ websit/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from websit.views import *
 
+app_name = "websit"
+
 urlpatterns = [
-    path('',index_view),
-    path('about',about_view),
-    path('contact',contact_view)
+    path('',index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
+    path('elements',elements_view,name='elements')
 ]
