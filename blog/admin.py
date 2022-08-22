0f41a71c91c8126727f2ce06fce324089ec40f8d
Pooya Nazari <66327968+pooyanazarii@users.noreply.github.com>
2022-08-22 20:53:44 +0430

    added models and registered table
    Customize admin page table 'Post'

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from blog.models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin (admin.ModelAdmin):
+    date_hierarchy  = 'created_date'
+    empty_value_display = '-empty-'
+    # fields= ('title',)
+    list_display = ("title","counted_view","status","published_date","created_date")
+    list_filter = ["status"]
+    ordering = ('-created_date',)
+    search_fields = ['content','title']
+
+
+
+#admin.site.register(Post,PostAdmin)
